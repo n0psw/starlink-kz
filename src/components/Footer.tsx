a1f377db00f4167470e0fff849ea18f0c3dae8dc
Adilkhan <adilkhan@MacBook-Air-Adilkhan.local>
2026-03-10 11:28:59 +0500

    referral edit + video

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -13,12 +13,6 @@ const Footer = () => {
                 +7 700 700 6613
               </a>
               <a
-                href="tel:+77019444441"
-                className="block text-slate-700 hover:text-accent transition-colors mb-2"
-              >
-                +7 701 944 4441
-              </a>
-              <a
                 href="https://wa.me/77007006613"
                 target="_blank"
                 rel="noopener noreferrer"
