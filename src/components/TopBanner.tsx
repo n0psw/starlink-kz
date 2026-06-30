d6f325737a02ed619102b5f913b1fd98ad87836c
n0psw <108538929+n0psw@users.noreply.github.com>
2026-07-01 01:53:03 +0500

    Update TopBanner.tsx

--- src/components/TopBanner.tsx
+++ src/components/TopBanner.tsx
@@ -10,7 +10,7 @@ const TopBanner = () => {
     >
       <div className="container mx-auto px-3 md:px-4 text-center">
         <a
-          href="https://starlink.com/residential?referral=RC-DF-12629508-59794-13"
+          href="https://starlink.com/residential?referral=RC-DF-13176434-89342-1"
           target="_blank"
           rel="noopener noreferrer"
           className="text-[11px] sm:text-xs md:text-sm text-slate-700 font-semibold hover:text-accent transition-colors inline-block leading-tight"
